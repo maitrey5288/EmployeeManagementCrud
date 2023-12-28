661f99474ec50e7be951ddf9edeba2fa3760a0ca
maitrey5288 <maitrey.chitale21@vit.edu>
2023-12-28 13:29:30 +0530

    cors problem solved

--- EmployeeManagement/src/main/java/com/maitrey/EmployeeManagement/controller/EmployeeController.java
+++ EmployeeManagement/src/main/java/com/maitrey/EmployeeManagement/controller/EmployeeController.java
@@ -12,7 +12,7 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-@CrossOrigin(origins = "http://localhost:3000")
+@CrossOrigin(origins = "https://employee-management-crud-theta.vercel.app/")
 @RestController
 @RequestMapping("/api/v1/")
 public class EmployeeController {
